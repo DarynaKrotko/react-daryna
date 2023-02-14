ca72fca9cf3e2ba834eeb06e3e219b118c074faa
DarynaKrotko <barert454@gmail.com>
2023-02-14 17:35:07 +0200

    homeworkTS1

---
+++ app.ts
@@ -0,0 +1,78 @@
+// 1) створити інтерфейс на основі цього объекта:
+//     Зверніть увагу там де масиви... в них може бути багато однотипних обїектів
+interface IStarlink {
+    mission_name: string,
+    launch_date_local: string,
+    launch_site: {
+        site_name_long:string
+    },
+    links: {
+        article_link: null,
+        video_link: string
+    },
+    rocket: {
+        rocket_name: string,
+        first_stage: {
+            cores: [
+                {
+                    flight: number,
+                    core: {
+                        reuse_count: number,
+                        status:string
+                    }
+                }
+            ]
+        },
+        second_stage: {
+            payloads: [
+                {
+                    payload_type: string,
+                    payload_mass_kg: number,
+                    payload_mass_lbs: number
+                }
+            ]
+        }
+    }
+}
+
+// 2) протипізувати функції:
+
+const user = {
+    name:"Max",
+    age:18,
+    gender:'male'
+}
+interface IUser {
+    name:string;
+    age:number;
+    gender:string;
+}
+
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(sum(1, 2));
+function sum(a:number,b:number):number{
+    return a+b
+}
+console.log(sum(1, 2));
+
+// function showSum(a,b){
+//     console.log(a + b);
+// }
+// showSum(2,3)
+function showSum(a:number,b:number):void{
+    console.log(a + b);
+}
+showSum(2,3)
+
+// function incAge(someUser, inc){
+//     someUser.age+=inc
+//     return someUser
+// }
+// incAge(user, 2)
+function incAge(someUser:IUser, inc:number):IUser{
+    someUser.age+=inc
+    return someUser
+}
+console.log(incAge(user, 2));
